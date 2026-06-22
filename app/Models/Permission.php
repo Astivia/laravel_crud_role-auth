08a023facc371ccb496c0939857a96d8a3aca5e8
Astivia <maximiliano.astivia@ciss-bienestar.org>
2026-06-21 23:12:10 -0600

    definir modelos y relaciones personalizadas

---
+++ app/Models/Permission.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name','description'];
+
+    //foreign key to roles_has_permission table
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'roles_has_permission', 'id_permission', 'id_role')
+                    ->withTimestamps();
+    }
+
+}
